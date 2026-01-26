8d914ce5fdf290c5f3068cbeec6f8829541a25b7
Shawn Morgan <shawnjr04@gmail.com>
2026-01-26 14:04:42 -0500

    Module 1 Assignment 1

---
+++ First.java
@@ -0,0 +1,9 @@
+// I looked back at some of the stuff I did last semester to help with this assignment.
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Job Title: Computer Network Architect");
+        System.out.println("Why: Good Pay");
+        System.out.println("Median Salary: $130,390");
+    }
+}
