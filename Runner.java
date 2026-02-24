c4b10620f280e59544c6d14a36c612c9cb527f15
Shawn Morgan <shawnjr04@gmail.com>
2026-02-23 22:33:49 -0500

    Module 3 Assignment 2 (Updated)
    Both files didn't show up at first.

--- Runner.java
+++ Runner.java
@@ -1,31 +1,10 @@
-import java.util.Scanner;
-
 public class Runner {
     public static void main(String[] args) {
 
-        Scanner scnr = new Scanner(System.in);
+        Tests myTests = new Tests();
 
-        // Pet 1 — default constructor
-        Pet myPet1 = new Pet();
-        System.out.println(myPet1.toString());
+        myTests.getAverage();
 
-        // Pet 2 — custom constructor
-        Pet myPet2 = new Pet("Dog", "Buster", 11);
-        System.out.println(myPet2.toString());
-
-        // Pet 3 — user input
-        System.out.println("Enter animal type:");
-        String type = scnr.nextLine();
-
-        System.out.println("Enter animal name:");
-        String name = scnr.nextLine();
-
-        System.out.println("Enter animal age:");
-        int age = scnr.nextInt();
-
-        Pet myPet3 = new Pet(type, name, age);
-        System.out.println(myPet3.toString());
-
-        scnr.close();
+        System.out.println(myTests.toString());
     }
 }
