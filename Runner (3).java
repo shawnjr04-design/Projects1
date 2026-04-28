f9225aed8cb1fccc96397492a913cfd876597f7c
Shawn Morgan <shawnjr04@gmail.com>
2026-04-28 03:32:30 -0400

    Module 7 Assignment 1
    Sorry this took so long. Was busy working on a major project that took me longer than expected.

--- Runner (3).java
+++ Runner (3).java
@@ -9,8 +9,10 @@ public class Runner {
         public static void main(String[] args)
         {
             String input;     // To hold input
-            int questions;    // Number of questions
-            int missed;       // Number of questions missed
+            int questions = 0;    // Number of questions
+            int missed = 0; 
+            
+            try {// Number of questions missed
 
             // Get the number of questions on the exam
             input = JOptionPane.showInputDialog("How many questions are on the exam?");
@@ -20,6 +22,10 @@ public class Runner {
             input = JOptionPane.showInputDialog("How many questions did the student miss?");
             missed = Integer.parseInt(input);
 
+            if (questions == 0) {
+                throw new ArithmeticException("Number of questions can't be zero.");
+            }
+
             // Create an Exam object
             Exam exam = new Exam(questions, missed);
 
@@ -28,6 +34,18 @@ public class Runner {
             message += " points.\nThe exam score is " + exam.getScore();
             message += "\nThe exam grade is " + exam.getGrade();
             JOptionPane.showMessageDialog(null, message);
+
+        }
+        catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null,
+                "Math error: " + e.getMessage());
+            
+        }
+        catch (ArithmeticException e) {
+            JOptionPane.showMessageDialog(null,
+                "Error: Can't divide by zero or exam setup is invalid.");
+            
+        }
 
             System.exit(0);
         }
