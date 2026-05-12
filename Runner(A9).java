11653375fafacdd80417963b6f81f9a9c9892e83
Shawn Morgan <shawnjr04@gmail.com>
2026-05-11 21:44:45 -0400

    Module 9 Assignment 1 (Fixed)
    Fixed the runner.java file. Now it should show the correct file.

---
+++ Runner(A9).java
@@ -0,0 +1,8 @@
+public class Runner {
+    ///Instantiate a Towers object to swap the rings on 3 "towers"
+    public static void main(String[] args)
+    {
+        Towers towersOfHanoi = new Towers(3);
+    }
+
+}
